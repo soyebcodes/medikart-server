a27a87d64554d970e3c4191186c2e4df211028a9
soyebcodes <soyeb10191@gmail.com>
2025-07-09 22:21:17 +0600

    Basic setup of the project

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("MediKart API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
