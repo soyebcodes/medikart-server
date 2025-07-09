fa032b16e4da071472e402871bb55500dcf32e69
soyebcodes <soyeb10191@gmail.com>
2025-07-09 23:45:49 +0600

    fix User not getting issue

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const { upsertUser } = require("../controllers/userController");
+const User = require("../models/User");
 
 router.put("/:email", upsertUser); // Save user after login/signup
+
+// get user role based
+router.get("/role/:email", async (req, res) => {
+  const { email } = req.params;
+  const user = await User.findOne({ email });
+  if (user) {
+    res.send({ role: user.role });
+  } else {
+    res.status(404).send({ role: null });
+  }
+});
+
 
 module.exports = router;
