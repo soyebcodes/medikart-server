9c7861d16e753d14f26155fb29122463858139e9
soyebcodes <soyeb10191@gmail.com>
2025-07-10 09:20:08 +0600

    update db name

--- config/db.js
+++ config/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "medi-kart",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
